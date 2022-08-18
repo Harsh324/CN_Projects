55c9a22d8e5ff43a1db2f53188faf584c68cba3b
Harsh_324 <harshravi324@gmail.com>
2022-08-18 10:13:43 +0530

    Some files added

--- ChatApp/Server/Server.java
+++ ChatApp/Server/Server.java
@@ -1,5 +1,114 @@
 package ChatApp.Server;
 
-public class Server {
-    
+/*
+ * Importing Important packages
+ */
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/*
+ * This class is the main class to handle the Server operations
+ */
+public class Server
+{
+    // Set to store the Current running threads
+    private Set<Users> Threads = new HashSet<>();
+
+    // Set to store the Active thread's user_Id.
+    private Set<String> User_IDs = new HashSet<>();
+
+    // Variable to store the Port number
+    private int Port;
+
+    /*
+     * The Constructor of the Class Server that
+     * takes the port number as input
+     */
+    public Server(int Port)
+    {
+        this.Port = Port;
+    }
+
+    /*
+     * This method starts the server and continously
+     * waits for the client to send request and accept it.
+     */
+    public void Run()
+    {
+        try(ServerSocket Socket_server = new ServerSocket(Port))
+        {
+            System.out.println("Server is listening at the Port : " + this.Port);
+
+            while(true)
+            {
+                Socket socket = Socket_server.accept();
+                System.out.println("User Connected.");
+
+                Users User = new Users(socket, this);
+                Threads.add(User);
+                User.start();
+            }
+        }
+        catch(IOException Ex)
+        {
+            System.out.println("Error in connecting with the server in Server file" + Ex.getMessage());
+            Ex.printStackTrace();
+        }
+    }
+
+    /*
+     * Method to add User_ids to the User_ID Set.
+     */
+    void addUser_ids(String User_Id)
+    {
+		User_IDs.add(User_Id);
+	}
+
+
+    /*
+     * Method to remove the User_id from the User_Id Set along with
+     * Removing from the Thread Set.
+     */
+    void Remove_User(String User_Id, Users User) {
+		boolean Flag = User_IDs.remove(User_Id);
+		if(Flag) 
+        {
+			Threads.remove(User);
+			System.out.println("User :- [ " + User_Id + " ] has quitted.");
+		}
+	}
+
+    /*
+     * Method to Broadcastt message from 
+     * server to one of the client.
+     */
+    void Notify(String Message, Users excludeUser) 
+    {
+		for (Users It : Threads) 
+        {
+			if (It != excludeUser)
+            {
+				It.SendMessage(Message);
+			}
+		}
+	}
+
+
+    boolean IfUsers()
+    {
+        if(this.User_IDs.isEmpty())
+            return false;
+        return true;
+    }
+
+    public static void main(String args[])
+    {
+        int Port;
+        Port = Integer.parseInt(args[0]);
+
+        Server server = new Server(Port);
+        server.Run();
+    }
+
 }
---
+++ ChatApp/Server/Users.java
@@ -0,0 +1,71 @@
+package ChatApp.Server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Users extends Thread
+{
+    private Socket socket;
+	private Server server;
+	private PrintWriter writer;
+
+	public Users(Socket socket, Server server) 
+    {
+		this.socket = socket;
+		this.server = server;
+	}
+
+    public void Start()
+    {
+        try
+        {
+            InputStream Input = socket.getInputStream();
+            BufferedReader Reader = new BufferedReader(new InputStreamReader(Input));
+
+            OutputStream Output = socket.getOutputStream();
+			writer = new PrintWriter(Output, true);
+
+            String User_id = Reader.readLine();
+            server.addUser_ids(User_id);
+
+            String Message = "User Connected - " + User_id;
+            server.Notify(Message, this);
+
+            String Client_message = Reader.readLine();
+
+            while(!Client_message.equals("Thank You"))
+            {
+                Client_message = Reader.readLine();
+                Message = "[ " + User_id + " ] -> " + Client_message;
+                server.Notify(Message, this);
+            }
+
+            server.Remove_User(User_id, this);
+            socket.close();
+
+            Message = User_id + " has quitted ";
+            server.Notify(Message, this);
+
+        }
+        catch(IOException Ex)
+        {
+            System.out.println("Error in connecting with the server int Users file" + Ex.getMessage());
+            Ex.printStackTrace();
+        }
+    }
+
+    void SendMessage(String message) 
+    {
+		writer.println(message);
+	}
+
+    
+    void PrintUsers()
+    {
+        if(server.IfUsers())
+        {
+            writer.println("Active users are : ");
+        }
+    }
+}
